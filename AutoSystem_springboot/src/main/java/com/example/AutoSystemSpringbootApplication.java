8b980d6f02abe033dcf683a7d98dd0b6751642b8
Mihara_Mika <m_mihara@emyus.jp>
2021-04-08 15:08:29 +0900

    2021_04_08 初めてのコミット

---
+++ AutoSystem_springboot/src/main/java/com/example/AutoSystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSystemSpringbootApplication.class, args);
+	}
+
+}
