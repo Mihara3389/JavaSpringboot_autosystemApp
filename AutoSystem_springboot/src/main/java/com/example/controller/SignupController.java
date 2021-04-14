a6b1dce011f4897535e44c088629bba34f118525
Mihara_Mika <m_mihara@emyus.jp>
2021-04-14 14:46:19 +0900

    新規登録画面にてユーザ名がデータベースに登録済である場合にエラーメッセージ表示機能実装 ※不要ロジックやクラス削除

--- AutoSystem_springboot/src/main/java/com/example/controller/SignupController.java
+++ AutoSystem_springboot/src/main/java/com/example/controller/SignupController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.dto.SignupRequest;
-import com.example.service.SignupService;
+import com.example.entity.UserEntity;
+import com.example.repository.UserRepository;
 
 /**
  * ユーザー情報 Controller
@@ -27,8 +30,12 @@ public class SignupController {
   /**
    * ユーザー情報 Service
    */
+
   @Autowired
-  SignupService userService;
+  private UserRepository userRepository;
+  
+  @Autowired
+  PasswordEncoder passwordEncoder;
 
   /**
    * ユーザー新規登録画面を表示
@@ -58,23 +65,45 @@ public class SignupController {
    * @param model Model
    * @return ユーザー情報一覧画面
    */
-  @RequestMapping(value = "/signup", method = RequestMethod.POST)
-  public String create(@Validated @ModelAttribute SignupRequest userRequest, BindingResult result, Model model) {
-    if (result.hasErrors()) {
-      List<String> errorList = new ArrayList<String>();
-      for (ObjectError error : result.getAllErrors()) {
-        errorList.add(error.getDefaultMessage());
-      }
+   @RequestMapping(value = "/signup", method = RequestMethod.POST)
+   public String create(@Validated @ModelAttribute SignupRequest userRequest, BindingResult result, Model model) {
+     //入力値のチェック
+     if (result.hasErrors()) {
+       List<String> errorList = new ArrayList<String>();
+       for (ObjectError error : result.getAllErrors()) {
+         errorList.add(error.getDefaultMessage());
+       }
+       model.addAttribute("validationError", errorList);
+       return "signup";
+       
+     } else {
+     //データベースに一致するユーザがいるかチェック
+ 	  UserEntity userEqual = userRepository.findByUsernameEquals(userRequest.getUsername());
+ 	  
+ 	  if(userEqual != null)
+ 	  {
+ 		  System.out.println("Username has");
+ 		 // データベース上にすでに存在する
+ 		 model.addAttribute("validationError","*入力された名前はすでに登録済です。");
+ 		 return "signup";
+ 		  
+ 	  }else {
+ 		
+ 		  System.out.println("Username not has");
 
-      model.addAttribute("validationError", errorList);
-      return "signup";
-    }else {
-    	
-    	
-    }
-
-    // ユーザー情報の登録
-    userService.create(userRequest);
-    return "redirect:/login";
-  }
+ 		  Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+ 		  UserEntity user = new UserEntity();
+ 		  
+ 		  user.setUsername(userRequest.getUsername());
+ 		  //パスワードをハッシュ化して渡すオブジェクトにセット。
+ 		  user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+ 		  user.setCreatedAt(timestamp);
+ 		  user.setUpdatedAt(timestamp);
+ 		  userRepository.save(user);
+ 		  
+ 		  // ユーザー情報の登録
+ 		  return "redirect:/login";
+ 	  }   	
+     }
+   }
 }
