315bbf12e84c7c91fffbcb13c4d44d74f2f22e17
Mihara_Mika <m_mihara@emyus.jp>
2021-04-23 16:53:21 +0900

    Commonを新規作成し、同一処理を共通クラスにて実施

---
+++ AutoSystem_springboot/src/main/java/com/example/Common.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.dto.ListForm;
+import com.example.entity.AnswersEntity;
+import com.example.entity.QuestionsEntity;
+
+public class Common
+{
+	public ArrayList<ListForm> toCommon(List<QuestionsEntity> questionsEntity,List<AnswersEntity> answerEntity, ArrayList<ListForm> returnlist)
+	{
+		//変数定義
+		int db_questionsId =0;
+		int db_answersId =0;
+		int count =0;
+		//questionsループ
+		for(int i = 0; i < questionsEntity.size(); i++) 
+		{
+			//カウント初期化
+			count = 0;
+			//問題を取得
+			db_questionsId = questionsEntity.get(i).getId();
+			//answersループ
+			for(int j = 0; j < answerEntity.size(); j++)
+			{
+				//nullチェック
+				AnswersEntity answer = answerEntity.get(j); 
+				if (answer == null)
+				{ 
+					continue; 
+				}
+				//照合
+				db_answersId = answerEntity.get(j).getQuestion_id();
+				if(db_questionsId == db_answersId) 
+				{
+					//箱を新しくする(じゃないと同じ箱を使いまわしリスト状態にならない）
+					ListForm list = new ListForm();
+					//同じ問題IDの答え数
+					count = count+1;
+					//値をつめる
+					list.setId(db_questionsId);
+					list.setQuestion(questionsEntity.get(i).getQuestion());
+					list.setAnswer_id(count);	
+					list.setAnswer(answerEntity.get(j).getAnswer());	
+					//リストへつめる
+					returnlist.add(list);
+				}
+			}
+		}
+		return returnlist;
+	}
+}
+		
--- AutoSystem_springboot/src/main/java/com/example/controller/TopContoroller.java
+++ AutoSystem_springboot/src/main/java/com/example/controller/TopContoroller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Common;
 import com.example.dto.ListForm;
 import com.example.entity.AnswersEntity;
 import com.example.entity.HistoryEntity;
@@ -46,48 +47,21 @@ import com.example.service.QuestionsService;
 		//質問・答えをDBから取得
 		List<QuestionsEntity> questionsEntity = questionsService.searchAll();
 		List<AnswersEntity> answerEntity = answersService.searchAll();
-		//変数定義
-		int db_questionsId =0;
-		int db_answersId =0;
-		int count =0;
-		//questionsループ
-		for(int i = 0; i < questionsEntity.size(); i++) {
-			//カウント初期化
-			count = 0;
-			if(questionsEntity.isEmpty()) {
-				//問題・答えの新規登録画面へforward
-				return "register";
-			}else {
-				//問題を取得
-				db_questionsId = questionsEntity.get(i).getId();
-				//answersループ
-				for(int j = 0; j < answerEntity.size(); j++) {
-					//nullチェック
-					AnswersEntity answer = answerEntity.get(j); 
-					if (answer == null) { 
-						continue; 
-					}
-					//照合
-					db_answersId = answerEntity.get(j).getQuestion_id();
-					if(db_questionsId == db_answersId) {
-						//箱を新しくする(じゃないと同じ箱を使いまわしリスト状態にならない）
-						ListForm list = new ListForm();
-						//同じ問題IDの答え数
-						count = count+1;
-						//値をつめる
-						list.setId(db_questionsId);
-						list.setQuestion(questionsEntity.get(i).getQuestion());
-						list.setAnswer_id(count);	
-						list.setAnswer(answerEntity.get(j).getAnswer());	
-						//リストへつめる
-						listForm.add(list);
-					}
-				}
-			}
+		//質問がDBに一軒もなかったら、新規登録画面へ
+		if(questionsEntity.isEmpty()) 
+		{
+			//問題・答えの新規登録画面へforward
+			return "register";
+		}else 
+		{
+			//問題を取得
+			Common com = new Common();
+			listForm = com.toCommon(questionsEntity, answerEntity, listForm);
 		}
-		model.addAttribute("listForm", listForm);
+		model.addAttribute(listForm);
+		//問題一覧画面へ戻る
 		return "list";
-	}
+	    }
 	/**
 	 * テストを表示
 	 * @param model Model
